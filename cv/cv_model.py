edcf5a5ecc09cab6b88ddd226ef6b7663c3c4e5c
mikehquan19 <hoangphucquan19@gmail.com>
2026-03-23 01:49:09 -0500

    Reduce the model size, plan on quantizing the model further after training on subset of AffectNet

--- cv/cv_model.py
+++ cv/cv_model.py
@@ -6,17 +6,14 @@ def get_resnet():
     modified_resnet = resnet50(weights=ResNet50_Weights.DEFAULT)
     # Modify the linear head
     modified_resnet.fc = nn.Sequential(
-        nn.Linear(modified_resnet.fc.in_features, 4096, bias=True),
-        nn.ReLU(inplace=True),
-        nn.Dropout(),
-        nn.Linear(4096, 1024, bias=True),
+        nn.Linear(modified_resnet.fc.in_features, 1024, bias=True),
         nn.ReLU(inplace=True),
         nn.Dropout(),
         nn.Linear(1024, 7, bias=True)
     )
 
     # Freeze every layer of the model except linear head and last convolutions
-    trainable_layers = ["fc", "layer4", "layer3.3", "layer3.2"]
+    trainable_layers = ["fc", "layer4", "layer3.3"]
     for name, param in modified_resnet.named_parameters():
         trainable = False
         for trainable_layer in trainable_layers:
--- cv/fer2013.py
+++ cv/fer2013.py
@@ -5,10 +5,11 @@ from pathlib import Path
 import random
 from PIL import Image
 
-TRAIN_VAL_SPLIT_RATE = 0.90 # 90/10 train val split
+TRAIN_VAL_SPLIT_RATE = 0.90
+SEED_NUMBER = 42
 
 class FER2013(Dataset):
-    def __init__(self, set_type: str="train"):
+    def __init__(self, data_dir: str, set_type: str="train"):
         if set_type not in ["train", "val", "test"]:
             raise ValueError("Invalid dataset type.")
 
@@ -23,24 +24,23 @@ class FER2013(Dataset):
                 v2.RandomResizedCrop(size=224, scale=(0.8, 1), ratio=(1.0, 1.0)),
                 v2.ToImage(),
                 v2.ToDtype(torch.float32, scale=True),
-                v2.Normalize(
-                    # Mean and stdev used for ImageNet
-                    mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225]
-                )
+                v2.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225])
             ])
         else:
             self.transform = v2.Compose([
                 v2.Resize(224),
                 v2.ToImage(),
                 v2.ToDtype(torch.float32, scale=True),
-                v2.Normalize(
-                    mean=[0.485, 0.456, 0.406],
-                    std=[0.229, 0.224, 0.225]
-                )
+                v2.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225])
             ])
 
-        root = Path("/content/emotions/train") if set_type != "test" else Path("/content/emotions/test")
+        if set_type != "test":
+            root = Path(f"{data_dir}/train")
+        else:
+            root = Path(f"{data_dir}/test")
+
         for idx, class_dir in enumerate(root.glob("*")):
             # Last element of the filepath
             label = class_dir.name
@@ -49,10 +49,14 @@ class FER2013(Dataset):
             self.emotion_to_idx[label] = idx
 
         if set_type != "test":
-            random.seed(42) # To make training re-producible
+            # To make training re-producible, feel free to adjust as requested
+            random.seed(SEED_NUMBER)
             random.shuffle(self.data)
             split = int(len(self.data) * TRAIN_VAL_SPLIT_RATE)
-            self.data = self.data[:split] if set_type == "train" else self.data[split:]
+            if set_type == "train":
+                self.data = self.data[:split]
+            else:
+                self.data = self.data[split:]
 
 
     def __len__(self):
@@ -60,6 +64,7 @@ class FER2013(Dataset):
 
 
     def __getitem__(self, index):
+        """Load the image from the fileimage"""
         filename, label = self.data[index]
         img = Image.open(filename).convert("RGB")
         processed_img = self.transform(img)
