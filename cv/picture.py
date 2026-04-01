8c6c322ae9cb2736a161b730cd8391bab189381b
mikehquan19 <hoangphucquan19@gmail.com>
2026-04-01 14:58:23 -0500

    Replace with easier model

--- cv/picture.py
+++ cv/picture.py
@@ -4,18 +4,17 @@ import cv2
 from cv2.typing import MatLike
 from pathlib import Path
 import torch
+from torch import nn
 from torch import device, cuda, Tensor, float32
 from torchvision.transforms import v2
-from torchvision.models import Resnet
-from cv.cv_model import get_resnet
-from cv.train_classifier import load_model
+from torchvision.models import EfficientNet, efficientnet_b2
+from PIL import Image
 
 DEVICE: device = "cuda" if cuda.is_available() else "cpu"
 DETECTOR_FILEPATH = "./yolov8n-face-lindevs.pt"
-CLASSIFIER_FILEPATH = "./emotions_model.pt"
+CLASSIFIER_FILEPATH = "./emotions_model.pth"
 
 AVAILABLE_EMOTIONS = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
-TEXT_COLOR = cv2.FONT_HERSHEY_SIMPLEX
 NUM_TOP_EMOTIONS = 3
 
 # Maybe a better way of handling these constants rather than CONSTANT
@@ -39,27 +38,39 @@ class CVPipeline:
         detector = YOLO(filepath).to(device)
         return detector
     
+    def _init_emotion_detector(self, filepath: str, device: device) -> EfficientNet:
+        """Helper method: Initialize the emotional classification model"""
+        efficientnet = efficientnet_b2()
+        # Modify the linear head
+        efficientnet.classifier = nn.Sequential(
+            nn.Linear(1408, 2048, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(2048, 512, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(512, 7, bias=True)
+        )
 
-    def _init_emotion_classifier(self, filepath: str, device) -> Resnet:
-        """Get the Resnet-based emotion classifying model"""
-        classifier = get_resnet().to(device).eval()
-        classifier.load_state_dict(torch.load(filepath, map_location=device))
-        return classifier
+        efficientnet.load_state_dict(torch.load(filepath, map_location=device))
+        return efficientnet.to(device)
 
 
     def _convert_to_tensor(image: MatLike, device: device) -> Tensor:
         """Convert the cv2 image to torch tensor"""
-        # Convert img to 1-dim grayscale first, then 3-dim grayscale
-        grayscale = cv2.cvtColor(cv2.cvtColor(image, cv2.COLOR_BGR2GRAY), cv2.COLOR_GRAY2BGR)
-        # Scale, normalize the tensorized img
+        modified = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  
+        modified = Image.fromarray(modified)
+
         transform = v2.Compose([
-            v2.Resize(224),
+            v2.Lambda(lambda x: x.convert("L").convert("RGB")),
+            v2.Resize([224, 224]),
             v2.ToImage(),
-            v2.ToDtype(float32, scale=True),
+            v2.ToDtype(torch.float32, scale=True),
             v2.Normalize(mean=[0.485, 0.456, 0.406],
-                        std=[0.229, 0.224, 0.225])
+                            std=[0.229, 0.224, 0.225])
         ])
-        return transform(grayscale).unsqueeze(0).to(device)
+
+        return transform(modified).unsqueeze(0).to(device)
     
 
     def get_camera(self):
@@ -127,7 +138,20 @@ class CVPipeline:
             boxes = analysis[0].boxes.xyxy.cpu().numpy()
             if boxes and isinstance(boxes, list) and len(boxes) > 0:
                 # The face the YOLO is most confident, which I think is often the closest face
+                h, w, _ = image.shape
                 x1, y1, x2, y2 = map(int, boxes[0])
+                box_w = x2 - x1
+                box_h = y2 - y1
+                side = max(box_w, box_h)
+
+                cx = (x1 + x2) // 2
+                cy = (y1 + y2) // 2
+
+                x1 = max(0, cx - side // 2)
+                y1 = max(0, cy - side // 2)
+                x2 = min(w, cx + side // 2)
+                y2 = min(h, cy + side // 2)
+                face_region: MatLike = image[y1:y2, x1:x2]
 
                 # Feed to emotion classifier
                 face_region = image[y1:y2, x1:x2]
@@ -135,17 +159,18 @@ class CVPipeline:
                     output = self.emotion_classifier(self._convert_to_tensor(face_region, DEVICE))
                     _, top_labels = torch.topk(output, NUM_TOP_EMOTIONS)
 
-                emotions = [AVAILABLE_EMOTIONS[label] for label in top_labels[0].cpu().numpy()]
+                emotions = [AVAILABLE_EMOTIONS[l] for l in top_labels[0].cpu().numpy()]
                 cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                cv2.putText(image, emotions, (10, 30), TEXT_COLOR, 0.9, (0, 255, 0), 2)
-
+                cv2.putText(
+                    image, emotions, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
             else: 
                 emotions = ["No emotion determined"]
-                cv2.putText(image, emotions, (10, 30), TEXT_COLOR, 0.7, (0, 0, 255), 2)
+                cv2.putText(
+                    image, emotions, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
                 
         except Exception as e:
             emotions = [f"Error during emotional analysis {e}"]
-            cv2.putText(image, emotions, (10, 30), TEXT_COLOR, 0.7, (0, 0, 255), 2)
+            cv2.putText(image, emotions, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 
         image_path = CHILD_PATH / "analyzed_image.jpg"
         cv2.imwrite(str(image_path), image)
@@ -154,7 +179,7 @@ class CVPipeline:
         return emotions
 
 
-def main():
+if __name__ == "__main__": 
     # This is to test the workflow of the pipeline when integrated into workflow
     cv_pipeline = CVPipeline()
     conversation_mode = False
@@ -172,6 +197,4 @@ def main():
         if "bye" in prompt.lower():
             conversation_mode = False
             cv_pipeline.turn_off_camera()
-
-if __name__ == "__main__": 
-    main()
+            break
