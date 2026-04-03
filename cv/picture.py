afcd1a6a1784adfb8f982256071b2d59834a7493
mikehquan19 <hoangphucquan19@gmail.com>
2026-04-03 00:36:49 -0500

    Fully working emotion-detection, as well as movement tracking. Will work on integrating them together

--- cv/picture.py
+++ cv/picture.py
@@ -5,72 +5,88 @@ from cv2.typing import MatLike
 from pathlib import Path
 import torch
 from torch import nn
-from torch import device, cuda, Tensor, float32
+from torch import device, cuda, Tensor
 from torchvision.transforms import v2
 from torchvision.models import EfficientNet, efficientnet_b2
 from PIL import Image
 import time
-import serial # type: ignore
 
+# FEATURE FLAG (Sort of)
+CONNECT_ARDUINO_FLAG: bool = False
+
+# GLOBAL
 DEVICE: device = "cuda" if cuda.is_available() else "cpu"
 PERSON_DETECTOR_FILEPATH = "yolov8n.pt"
 FACE_DETECTOR_FILEPATH = "./yolov8n-face-lindevs.pt"
-CLASSIFIER_FILEPATH = "./emotions_model.pth"
-
-AVAILABLE_EMOTIONS = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
+EMOTION_CLASSIFIER_FILEPATH = "./emotions_model.pt"
+AVAILABLE_EMOTIONS = ["Angry", "Fear", "Happy", "Neutral", "Sad"]
 NUM_TOP_EMOTIONS = 3
 
-# Maybe a better way of handling these constants rather than CONSTANT
-PARENT_NAME = r"C:\Users\sahot\OneDrive\Desktop\textbooks+Slides\EPICS\spring26\code\BrainCharge_Update\BrainCharge"
-DIRECTORY_NAME = "picturefile"
+# Check and create if needed the file needed for the file
+try:
+    DIRECTORY_NAME = "./picturefile"
+    DIRECTORY_PATH = Path(DIRECTORY_NAME)
 
-PARENT_PATH = Path(PARENT_NAME)
-CHILD_PATH = PARENT_PATH / DIRECTORY_NAME
-
-CHILD_PATH.mkdir(parents=True, exist_ok=True)
-print(f"Image folder ready at: {CHILD_PATH}")
+    DIRECTORY_PATH.mkdir(parents=True, exist_ok=True)
+    print(f"Image folder ready at: {DIRECTORY_NAME}")
+except Exception as e:
+    raise Exception("Error finding or creating directory to store images: ", e)
 
 class CVPipeline:
     def __init__(self):
         self.camera: Optional[cv2.VideoCapture] = None
-        self.person_detector = self._init_detector(PERSON_DETECTOR_FILEPATH, device)
-        self.face_detector = self._init_face_detector(FACE_DETECTOR_FILEPATH, DEVICE)
-        self.emotion_classifier = self._init_emotion_classifier(CLASSIFIER_FILEPATH, DEVICE)
+        self.person_detector = self._init_detector(
+            PERSON_DETECTOR_FILEPATH, 
+            DEVICE
+        )
+        self.face_detector = self._init_detector(
+            FACE_DETECTOR_FILEPATH, 
+            DEVICE
+        )
+        self.emotion_classifier = self._init_emotion_classifier(
+            EMOTION_CLASSIFIER_FILEPATH, 
+            DEVICE
+        )
+        self.emotion_classifier.eval()
 
-        self.arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=2)
-        time.sleep(2)  # Wait for Arduino reset
+        # For person tracking
+        self.target: Optional[int] = None
 
-    def _init_face_detector(self, filepath: str, device) -> YOLO:
-        """Get the YOLO-based detection model"""
+        if CONNECT_ARDUINO_FLAG:
+            import serial # type: ignore
+            self.arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=2)
+            time.sleep(2)  # Wait for Arduino reset
+
+
+    def _init_detector(self, filepath: str, device) -> YOLO:
+        """Helper method: Get the YOLO-based detection model"""
         detector = YOLO(filepath).to(device)
         return detector
     
-    def _init_emotion_detector(self, filepath: str, device: device) -> EfficientNet:
+
+    def _init_emotion_classifier(self, filepath: str, device: device) -> EfficientNet:
         """Helper method: Initialize the emotional classification model"""
         efficientnet = efficientnet_b2()
         # Modify the linear head
         efficientnet.classifier = nn.Sequential(
-            nn.Linear(1408, 2048, bias=True),
+            nn.Linear(1408, 512, bias=True),
             nn.ReLU(inplace=True),
             nn.Dropout(),
-            nn.Linear(2048, 512, bias=True),
-            nn.ReLU(inplace=True),
-            nn.Dropout(),
-            nn.Linear(512, 7, bias=True)
+            nn.Linear(512, 5, bias=True)
         )
 
         efficientnet.load_state_dict(torch.load(filepath, map_location=device))
         return efficientnet.to(device)
 
 
-    def _convert_to_tensor(image: MatLike, device: device) -> Tensor:
-        """Convert the cv2 image to torch tensor"""
+    def _convert_to_tensor(self, image: MatLike, device: device) -> Tensor:
+        """Helper method: Convert the cv2 image to torch tensor"""
         modified = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  
         modified = Image.fromarray(modified)
 
         transform = v2.Compose([
             v2.Lambda(lambda x: x.convert("L").convert("RGB")),
-            v2.Resize([224, 224]),
+            v2.Resize([288, 288]),
             v2.ToImage(),
             v2.ToDtype(torch.float32, scale=True),
             v2.Normalize(mean=[0.485, 0.456, 0.406],
@@ -80,9 +96,9 @@ class CVPipeline:
         return transform(modified).unsqueeze(0).to(device)
     
 
-    def get_camera(self):
-        """Get the camera (OOP 101)"""
-        return self.camera
+    def camera_on(self) -> bool:
+        """Check if the camera is on"""
+        return self.camera is not None
     
 
     def turn_on_camera(self) -> None:
@@ -113,10 +129,15 @@ class CVPipeline:
             print("3. No other application is using camera")
             exit()
 
+        # Warm up the camera to prevent it from crashing when reopening
         for _ in range(7):
-            # Warm up the camera to prevent it from crashing when reopening
             self.camera.read()
         print("Turn on the camera successfully!")
+
+
+    def camera_off(self) -> bool:
+        """Check if the camera is off"""
+        return self.camera is None
 
 
     def turn_off_camera(self) -> None:
@@ -128,44 +149,51 @@ class CVPipeline:
             print("Turn off the camera successfully!")
 
 
-    # TODO: Find the better measurement than this
-    def _get_turn_signal(self, w, x1, x2) -> str:
-        """Determine if the robot turns left or right"""
-        center = (x1 + x2) / 2
-        threshold_left, threshold_right = w / 4, w * 3 / 4
+    def _get_turn_signal(self, w: int, x1: int, x2: int) -> str:
+        """Helper method: Determine if the robot turns left or right"""
+        center_dist = (x1 + x2 - w) / 2
+        threshold = w / 5
 
-        if center > threshold_right:
+        if center_dist > threshold:
             return "L"
-        elif center < threshold_left:
+        elif center_dist < -threshold:
             return "R"
         
         return "S"
-    
 
-    def _get_move_signal(self, w, h, x1, x2, y1, y2) -> str:
-        """Determine if the robot moves forward or backward"""
+
+    def _get_move_signal(self, 
+        w: int, h: int, x1: int, x2: int, y1: int, y2: int
+    ) -> str:
+        """Helper method: Determine if the robot moves forward or backward"""
         region_area = (y2 - y1) * (x2 - x1)
         camera_area = h * w
 
-        if region_area > camera_area / 2:
+        if region_area > camera_area / 3:
             return "B"
         elif region_area < camera_area / 4:
             return "F"
         
         return "S"
-    
 
-    def _send_command(self, cmd):
-        """Send the command to the adruino"""
+
+    def _send_command(self, cmd: str):
+        # ABBY HELP ME WITH THIS!!
+        """Helper method: Send the new movement command to the adruino"""
         self.arduino.write((cmd + '\n').encode())
         return self.arduino.readline().decode().strip()
-    
 
-    def realtime_movement_tracking(self) -> tuple[int]:
+
+    def track_movement(self) -> tuple[int]:
         """
-        Person-tracking system for robot movement
+        Person-tracking system for robot movement.
+        It captures the first person to get in the frame and lock in that person as target to follow.
+        If the target gets out of frame before the robot's reaction, it will automatically choose
+        the next person it captures as the target.
+        
+        To adjust the target, clear the camera's frame and be the first person to be in the frame.
         """
-        curr_turn, curr_move = "", ""
+        prev_turn, prev_move = "", ""
 
         if not self.camera: 
             raise Exception("Unable to open the camera")
@@ -175,7 +203,12 @@ class CVPipeline:
             if not success:
                 raise ValueError("Failed to capture the image of the user")
             
-            analysis = self.person_detector.track(image, persist=True, tracker="bytetrack.yaml")[0].boxes
+            # The object-detection model but uses bytetrack algorithm to assign id to each detected obj
+            analysis = self.person_detector.track(
+                image, 
+                persist=True, 
+                tracker="bytetrack.yaml"
+            )[0].boxes
             if analysis.id is None:
                 # No persons or objects to track, send the empty image
                 cv2.imshow("Realtime Personal Tracking", image)
@@ -191,16 +224,19 @@ class CVPipeline:
                 self.target is not None and 
                 self.target not in [int(id) for id in track_ids]
             ):
-                # Clear the target if the target is out
+                # Clear the target if the target is out of the frame
                 self.target = None
 
-            for box, cls, track_id in zip(boxes, classes, track_ids):
-                if int(cls) == 0 and self.target is None:
+            for box, class_id, track_id in zip(boxes, classes, track_ids):
+                # YOLOv8 can actually detect more than just a person, but person's class_id is 0
+                if int(class_id) == 0 and self.target is None:
                     # Lock in the new target if there is no previous targets
                     # or the previous target is out
                     self.target = int(track_id)
 
                 if self.target == int(track_id):
+                    # For the target, compute the geometric of the target compared with the image
+                    # and get the instruction
                     h, w, _ = image.shape
                     x1, y1, x2, y2 = map(int, box)
 
@@ -208,30 +244,38 @@ class CVPipeline:
                     new_turn = self._get_turn_signal(w, x1, x2)
                     new_move = self._get_move_signal(w, h, x1, x2, y1, y2)
 
-                    if new_turn != curr_turn or new_move != curr_move:
+                    if new_turn != prev_turn or new_move != prev_move:
                         # If the instruction changes, send the new instruction to Arduino
-                        curr_turn = new_turn
-                        curr_move = new_move
-                        self._send_command(f"{curr_move}, {curr_turn}")
+                        prev_turn = new_turn
+                        prev_move = new_move
+                        if CONNECT_ARDUINO_FLAG:
+                            self._send_command(f"{prev_move}, {prev_turn}")
 
                     cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
                     cv2.putText(
                         image, 
-                        f"{curr_move}, {curr_turn}", 
+                        f"Turn {prev_turn}, move {prev_move}", 
                         (x1, y1 - 10), 
                         cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2
                     )
 
             cv2.imshow("Realtime Personal Tracking", image)
             if cv2.waitKey(1) & 0xFF == ord('q'):
+                # Hit q to stop the movement tracking (For DEMO)
                 break
 
 
-    def _expand_face(self, w, h, x1, y1, x2, y2) -> tuple:
-        """Expand the box a little bit to match the training images"""
+    def _expand_face(self, 
+        w: int, h: int, x1: int, y1: int, x2: int, y2: int
+    ) -> tuple[int]:
+        """Helper method: Expand the box a little bit to match the training images"""
         box_w = x2 - x1
         box_h = y2 - y1
         side = max(box_w, box_h)
+
+        # Add padding to the box around the face
+        padding = 0.2
+        side = int(side * (1 + padding)) 
 
         cx = (x1 + x2) // 2
         cy = (y1 + y2) // 2
@@ -249,54 +293,65 @@ class CVPipeline:
 
         # Take the picture of the user with the opened camera
         if not self.camera: 
-            return
+            return emotions
         success, image = self.camera.read()
         if not success: 
-            return
+            return emotions
 
         try: 
-            # Only one image, so generally only one analysis returned
-            analysis = self.face_detector(image)[0]
+            # The face detector captures only one image at the time
+            analysis = self.face_detector(image)[0].boxes
+            boxes = analysis.xyxy.cpu().numpy()
 
-            boxes = analysis[0].boxes.xyxy.cpu().numpy()
-            if boxes and isinstance(boxes, list) and len(boxes) > 0:
-                # The face the YOLO is most confident, which I think is often the closest face
+            if boxes is not None:
+                # The face the YOLO is most confident, which is often the closest face
                 h, w, _ = image.shape
                 x1, y1, x2, y2 = map(int, boxes[0])
                 x1, y1, x2, y2 = self._expand_face(w, h, x1, y1, x2, y2)
                 face_region: MatLike = image[y1:y2, x1:x2]
 
-                # Feed to emotion classifier
-                face_region = image[y1:y2, x1:x2]
                 with torch.no_grad():
                     tensor = self._convert_to_tensor(face_region, DEVICE)
-                    output = self.emotion_classifier(tensor)
-                    _, top_labels = torch.topk(output, NUM_TOP_EMOTIONS)
-                    emotions = [AVAILABLE_EMOTIONS[l] for l in top_labels[0].cpu().numpy()]
+                    logits = self.emotion_classifier(tensor)
+                    probs = torch.softmax(logits, dim=1)
+                    top_probs, top_labels = torch.topk(probs, NUM_TOP_EMOTIONS)
+                    emotions = [
+                        {
+                            "emotion": AVAILABLE_EMOTIONS[l],
+                            "confidence": round(top_probs[0][i].item(), 2)
+                        } 
+                        for i, l in enumerate(top_labels[0].cpu().numpy())
+                    ]
                     
                 cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 cv2.putText(
                     image, 
-                    emotions, 
+                    ", ".join(e["emotion"] for e in emotions), 
                     (10, 30), 
                     cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2
                 )
+                image_path = DIRECTORY_PATH / "analyzed_image.jpg"
+                saved = cv2.imwrite(str(image_path), image)
+                if not saved:
+                    raise Exception("Error saving the image to the file")
+
+                print(f"Final analyzed image saved to: {image_path}")
             else: 
-                emotions = ["No emotion determined"]
                 cv2.putText(
-                    image, emotions, (
-                    10, 30), 
+                    image, 
+                    "No emotions determined", 
+                    (10, 30), 
                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2
                 )
                 
         except Exception as e:
-            emotions = [f"Error during emotional analysis {e}"]
-            cv2.putText(image, emotions, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            cv2.putText(
+                image, 
+                f"Error during emotional analysis {e}", 
+                (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2
+            )
 
-        image_path = CHILD_PATH / "analyzed_image.jpg"
-        cv2.imwrite(str(image_path), image)
-
-        print(f"Final analyzed image saved to: {image_path}")
         return emotions
 
 
@@ -307,15 +362,15 @@ if __name__ == "__main__":
 
     while True:
         prompt = input("Enter: ")
-        if "hello" in prompt.lower():
+        if "hello" in prompt.lower() and cv_pipeline.camera_off():
             conversation_mode = True
             cv_pipeline.turn_on_camera()
 
         if conversation_mode:
-            detected_emotions = cv_pipeline.execute()
-            print(f"LLM will response based on these emotions: {", ".join(detected_emotions)}")
+            emotions = cv_pipeline.execute()
+            print(f"Emotions: {", ".join([e["emotion"] for e in emotions])}")
 
-        if "bye" in prompt.lower():
+        if "bye" in prompt.lower() and cv_pipeline.camera_on():
             conversation_mode = False
             cv_pipeline.turn_off_camera()
             break
